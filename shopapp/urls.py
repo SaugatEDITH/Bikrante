d0f9e30614748350ffd52787218fd5680869e1f4
Saugat <saugatpokharel201@gmail.com>
2025-01-29 10:39:50 +0545

    single page multiform handle

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='log-out'),
+    path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('shop', views.shop, name='shop'),
+    path('shop/', views.shop, name='shop'),
     path('contact/', views.contact, name='contact'),
+    path('user-dashboard/',views.user_dashboard,name='user-dashboard'),
 ]
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 import re
 
 def home(request):
@@ -13,7 +17,7 @@ def shop(request):
     context = {
         'is_home': False
     }
-    return render(request, 'shopapp/products-card.html', context)
+    return render(request, 'shopapp/shop.html', context)
 
 def login(request):
     context = {
@@ -72,10 +76,50 @@ def signup(request):
         return redirect('home')
         print(f"Context messages: {context['message']}")  # Debug print
     return render(request, 'shopapp/login-register.html', context)
-def logout(request):
-    pass
-def shop(request):
-    return render(request,"shopapp/shop.html")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('home')
 
 def contact(request):
     return render(request, 'shopapp/contact.html')
+
+@login_required
+def user_dashboard(request):
+    context={
+        'message':[]
+    }
+    if request.method=="POST":
+        #handaling username Update
+        if 'update_username' in request.POST:
+            new_username=request.POST.get('username')
+            if User.objects.filter(username=new_username).exists():
+                context['message'].append("This username is already taken")
+                return redirect('user-dashboard#update-profile')
+            else:
+                request.user.username=new_username
+                request.user.save()
+                context['message'].append("Your username has been sucessfully updated.")
+                return redirect('user-dashboard')
+        elif 'change_password' in request.POST:
+            current_password=request.POST.get("current_password")
+            new_password=request.POST.get("new_password")
+            confirm_password=request.POST.get("confirm_password")
+            
+            user=authenticate(request.user.username,password=current_password)
+            if user:
+                if new_password==confirm_password:
+                    if not re.fullmatch(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$', new_password):
+                         context['message'].append("Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.")
+                         return redirect("user-dashboard#change-password")
+                    user.set_password(new_password)
+                    user.save()
+                    auth_login(request,user) #re-log user after password change
+                    context['message'].append("Your password has been sucessfully updated")
+                    return redirect("user-dashboard")
+                else:
+                    context['message'].append("New password and confirm password don't match.")
+            else:
+                context['message'].append("Current password is incorrect.")
+    return render(request,'shopapp/user-dashboard.html')
